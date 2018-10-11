66a7768b55681524e92b36963377e3d68eeb17a2
maoyutao <maoyutao1006@163.com>
2018-10-11 21:22:49 +0800

    complete API 2&3

--- userpage/views.py
+++ userpage/views.py
@@ -1,7 +1,9 @@
 from codex.baseerror import *
 from codex.baseview import APIView
 
-from wechat.models import User
+from wechat.models import *
+import time
+import datetime
 
 
 class UserBind(APIView):
@@ -23,3 +25,47 @@ class UserBind(APIView):
         self.validate_user()
         user.student_id = self.input['student_id']
         user.save()
+
+class ActivityDetail(APIView):
+
+    def get(self):
+        self.check_input('id')
+        act = Activity.objects.get(id=self.input['id'], status=1)
+        return {
+            'name': act.name,
+            'key': act.key,
+            'description': act.description,
+            'startTime': time.mktime(act.start_time.timetuple()),
+            'endTime': time.mktime(act.end_time.timetuple()),
+            'place': act.place,
+            'bookStart': time.mktime(act.book_start.timetuple()),
+            'bookEnd': time.mktime(act.book_end.timetuple()),
+            'totalTickets': act.total_tickets,
+            'picUrl': act.pic_url,
+            'remainTickets': act.remain_tickets,
+            'currentTime': time.mktime(datetime.datetime.now().timetuple()),
+        }
+
+class TicketDetail(APIView):
+    
+    def get(self):
+        self.check_input('openid', 'ticket')
+        student_id = User.objects.get(open_id=self.input['openid']).student_id
+        tic = Ticket.objects.get(student_id=student_id, unique_id=self.input['ticket'])
+        act = tic.activity
+        return {
+            'activityName': act.name,
+            'place': act.place,
+            'activityKey': act.key,
+            'uniqueId': tic.unique_id,
+            'startTime': time.mktime(act.start_time.timetuple()),
+            'endTime': time.mktime(act.end_time.timetuple()),
+            'currentTime': time.mktime(datetime.datetime.now().timetuple()),
+            'status': tic.status
+        }
+
+
+        
+
+
+        
