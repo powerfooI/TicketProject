21e75d4b11e1baa3de4a75671813de94ab4904ac
NaXin <naxin@NaXindeMacBook.local>
2018-10-16 02:58:27 +0800

    fix

--- wechat/wrapper.py
+++ wechat/wrapper.py
@@ -14,6 +14,7 @@ from django.template.loader import get_template
 from WeChatTicket import settings
 from codex.baseview import BaseView
 from wechat.models import User
+import re
 
 
 __author__ = "Epsirom"
