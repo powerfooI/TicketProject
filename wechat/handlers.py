4602c08674791e140fed04855d46bc199d8ffd99
Steve Yurong Su <steveyurongsu@outlook.com>
2018-10-18 23:39:22 +0800

    bug fix: try to fix some bugs in BookingActivityHandler

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -101,106 +101,94 @@ class BookingActivityHandler(WeChatHandler):
             return (self.input['Event'] == 'CLICK') and (self.input['EventKey'].startswith(self.view.event_keys['book_header']))
         return False
     
-    @transaction.atomic # 进行事务管理 
     def handle(self):
-        sid = transaction.savepoint() # 创建保存点
-        
-        # 测试是否存在此用户
-        try:
-            user = User.objects.select_for_update().get(open_id=self.user.open_id) # 悲观锁
-            # user = User.objects.get(open_id=self.user.open_id) # 悲观锁
-        except User.DoesNotExist:
-            transaction.rollback(sid)
-            raise BaseError(-1, 'User does not exist.')
+        with transaction.commit_on_success():            
+            # 测试是否存在此用户
+            try:
+                user = User.objects.select_for_update().get(open_id=self.user.open_id) # 悲观锁
+                # user = User.objects.get(open_id=self.user.open_id) # 悲观锁
+            except User.DoesNotExist:
+                raise BaseError(-1, 'User does not exist.')
 
-        # 测试用户是否绑定
-        if not user.student_id:
-            transaction.rollback(sid)
-            return self.reply_text(self.get_message('bind_account'))
+            # 测试用户是否绑定
+            if not user.student_id:
+                return self.reply_text(self.get_message('bind_account'))
 
-        # 处理文本信息的情况
-        if self.is_msg_type('text'):
-            act_key = self.input['Content'][len("抢票 "):]
-            acts = Activity.objects.select_for_update().filter(key=act_key)
-            # acts = Activity.objects.filter(key=act_key)
-        # 处理点击事件的情况
-        else:
-            act_id = int(self.input['EventKey'].split('_')[-1])
+            # 处理文本信息的情况
+            if self.is_msg_type('text'):
+                act_key = self.input['Content'][len("抢票 "):]
+                acts = Activity.objects.select_for_update().filter(key=act_key)
+                # acts = Activity.objects.filter(key=act_key)
+            # 处理点击事件的情况
+            else:
+                act_id = int(self.input['EventKey'].split('_')[-1])
+                acts = Activity.objects.select_for_update().filter(id=act_id)
+                # acts = Activity.objects.filter(id=act_id)
+
+            # 检查活动
+            if len(acts) == 0:
+                return self.reply_text("【 抢票失败 】 对不起，这儿没有对应的活动:(") 
+            act = acts[0]
+            if act.status != Activity.STATUS_PUBLISHED:
+                return self.reply_text("【 抢票失败 】 对不起，这儿没有对应的活动:(") 
+            current_timestamp = timezone.now().timestamp()
+            book_start_timestamp = act.book_start.timestamp()
+            book_end_timestamp = act.book_end.timestamp()
+            if current_timestamp < book_start_timestamp or book_end_timestamp < current_timestamp:
+                return self.reply_text("【 抢票失败 】 对不起，现在不是抢票时间:(") 
+            if act.remain_tickets <= 0:
+                return self.reply_text("【 抢票失败 】 对不起，已经没有余票了:(")
+
+            # 检查重复抢票的情况
+            tickets_valid_in_the_same_activity = Ticket.objects.select_for_update().filter(
+                student_id = user.student_id,
+                activity = act,
+                status = Ticket.STATUS_VALID
+            )
+            #tickets_valid_in_the_same_activity = Ticket.objects.filter(
+            #    student_id = user.student_id,
+            #    activity = act,
+            #    status = Ticket.STATUS_VALID
+            #)
+
+            if len(tickets_valid_in_the_same_activity) > 0:
+                tickets_valid_in_the_same_activity.save()
+                return self.reply_text("【 抢票失败 】 请不要重复抢票")
             
-            acts = Activity.objects.select_for_update().filter(id=act_id)
-            # acts = Activity.objects.filter(id=act_id)
+            tickets_used_in_the_same_activity = Ticket.objects.select_for_update().filter(
+                student_id = user.student_id,
+                activity = act,
+                status = Ticket.STATUS_USED
+            )
+            #tickets_used_in_the_same_activity = Ticket.objects.filter(
+            #    student_id = user.student_id,
+            #    activity = act,
+            #    status = Ticket.STATUS_USED
+            #)
+            if len(tickets_used_in_the_same_activity) > 0:
+                tickets_used_in_the_same_activity.save()
+                return self.reply_text("【 抢票失败 】 请不要重复抢票") 
+            
 
-        # 检查活动
-        if len(acts) == 0:
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 对不起，这儿没有对应的活动:(") 
-        act = acts[0]
-        if act.status != Activity.STATUS_PUBLISHED:
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 对不起，这儿没有对应的活动:(") 
-        current_timestamp = timezone.now().timestamp()
-        book_start_timestamp = act.book_start.timestamp()
-        book_end_timestamp = act.book_end.timestamp()
-        if current_timestamp < book_start_timestamp or book_end_timestamp < current_timestamp:
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 对不起，现在不是抢票时间:(") 
-        if act.remain_tickets <= 0:
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 对不起，已经没有余票了:(")
+            # 票充足，处理活动表格、电子票表格，失败、成功都则返回对应信息
+            act.remain_tickets = act.remain_tickets - 1
+            ticket_unique_id = str(uuid.uuid1()) + str(user.student_id)
+            if len(ticket_unique_id) > 64:
+                ticket_unique_id = ticket_unique_id[:64]
+            Ticket.objects.create(
+                student_id = user.student_id,
+                unique_id = ticket_unique_id,
+                activity = act,
+                status = Ticket.STATUS_VALID
+            )
 
-        # 检查重复抢票的情况
-        tickets_valid_in_the_same_activity = Ticket.objects.select_for_update().filter(
-             student_id = user.student_id,
-             activity = act,
-             status = Ticket.STATUS_VALID
-         )
-        #tickets_valid_in_the_same_activity = Ticket.objects.filter(
-        #    student_id = user.student_id,
-        #    activity = act,
-        #    status = Ticket.STATUS_VALID
-        #)
-
-        if len(tickets_valid_in_the_same_activity) > 0:
-            tickets_valid_in_the_same_activity.save()
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 请不要重复抢票")
-        
-        tickets_used_in_the_same_activity = Ticket.objects.select_for_update().filter(
-            student_id = user.student_id,
-            activity = act,
-            status = Ticket.STATUS_USED
-        )
-        #tickets_used_in_the_same_activity = Ticket.objects.filter(
-        #    student_id = user.student_id,
-        #    activity = act,
-        #    status = Ticket.STATUS_USED
-        #)
-        if len(tickets_used_in_the_same_activity) > 0:
-            tickets_used_in_the_same_activity.save()
-            transaction.rollback(sid)
-            return self.reply_text("【 抢票失败 】 请不要重复抢票") 
-        
-
-        # 票充足，处理活动表格、电子票表格，失败、成功都则返回对应信息
-        act.remain_tickets = act.remain_tickets - 1
-        ticket_unique_id = str(uuid.uuid1()) + str(user.student_id)
-        if len(ticket_unique_id) > 64:
-            ticket_unique_id = ticket_unique_id[:64]
-        Ticket.objects.create(
-            student_id = user.student_id,
-            unique_id = ticket_unique_id,
-            activity = act,
-            status = Ticket.STATUS_VALID
-        )
-
-        act.save()
-        user.save()
-        transaction.commit()
-        return self.reply_single_news({
-           'Title': "【 抢票成功 】 " + act.name,
-           'Description': act.description,
-           'Url': self.url_activity(act.id),
-        })
+            act.save()
+            user.save()
+            return self.reply_single_news({
+                'Title': "【 抢票成功 】 " + act.name,
+                'Description': act.description,
+                'Url': self.url_activity(act.id),
+            })
 
 
 class QueryTicketHandler(WeChatHandler):
